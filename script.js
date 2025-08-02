259c2c5a74ba1f834a24dae2cc6dad2e3b67cbcd
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-08-02 15:10:37 +0530

    slidier changes

---
+++ script.js
@@ -0,0 +1,50 @@
+ 
+const sliderItems = document.querySelectorAll('.slider-item');
+const buttons = document.querySelectorAll('.btn');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentSlide = 0;
+
+buttons.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        showSlide(i);
+    })
+}) 
+prevButton.addEventListener('click', () => {
+    // if current slider is at first item show last item
+    if(currentSlide === 0) {
+        showSlide(sliderItems.length - 1);
+    }
+    else {
+        showSlide(currentSlide - 1);
+    }
+})
+
+nextButton.addEventListener('click', () => {
+    // if current slider is at last item show first item
+   if(currentSlide === sliderItems.length -1){
+        showSlide(0);
+   }
+   else{
+        showSlide(currentSlide + 1);
+   }
+})
+function showSlide(index) {
+    // if there is a slide already active, remove the active class
+    sliderItems.forEach(item => {
+        item.classList.remove('active');
+    })
+    buttons.forEach(btn => {
+        btn.classList.remove('active');
+    })
+    // update current slide to  index
+    currentSlide = index;
+    // add active class to the current slide and button
+    sliderItems[currentSlide].classList.add('active');
+    buttons[currentSlide].classList.add('active');
+}
+
+showSlide(currentSlide); // calling the function to show the first slide
+
+
